01179431ada3042e802f10c86774f4ef3a8a2dae
Coral Marie <coraltribble@gmail.com>
2025-03-06 10:55:06 -0500

    update to add five pokemon to variable list

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,7 @@
-alert('Hello world & Jay!');
-let favoriteFood = 'Sushi';
-document.write(favoriteFood);
+var pokemonList=[
+    {name:'Pikachu', height:'.4', type:'electric'},
+    {name:'Charmander', height:'.6', type:'fire'},
+    {name:'Bulbasaur', height:'.7', type:['grass','poison']},
+    {name:'Squirtle', height:'.5', type:'water'},
+    {name:'Ditto', height:'.3', type:'normal'},
+];
